e65a7f3805fe0805fab9bda2aab26dc990734e0c
hannibal <hannibal0416@gmail.com>
2024-05-14 21:21:06 +0800

    remove userSvc

--- oauth2/src/main/java/com/cdk/modern/renting/oauth2/users/CustomUserDetailsService.java
+++ oauth2/src/main/java/com/cdk/modern/renting/oauth2/users/CustomUserDetailsService.java
@@ -1,28 +1,29 @@
 package com.cdk.modern.renting.oauth2.users;
 
 
-
 import com.cdk.modern.renting.oauth2.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserService userService;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getByUsername(username);
+  private final UserRepository userRepository;
 
-        if (user == null) {
-            throw new UsernameNotFoundException("Unable to found user: " + username);
-        }
-
-        return new CustomUserDetails(user);
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    if (!StringUtils.hasText(username)) {
+      return null;
     }
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Unable to found user: " + username));
+
+    return new CustomUserDetails(user);
+  }
 }
