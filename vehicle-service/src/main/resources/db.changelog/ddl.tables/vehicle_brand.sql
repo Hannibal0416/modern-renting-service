c872644e5a1004069e9338ee4fc9e661e2fafc49
nyotie-tao <nyoto.wibowo@taodigitalusa.com>
2024-05-16 13:49:11 +0800

    Add image-uri

--- vehicle-service/src/main/resources/db.changelog/ddl.tables/vehicle_brand.sql
+++ vehicle-service/src/main/resources/db.changelog/ddl.tables/vehicle_brand.sql
@@ -3,6 +3,7 @@
 --changeset application:1
 CREATE TABLE IF NOT EXISTS vehicle_brand (
     id SERIAL PRIMARY KEY,
+    image_uri VARCHAR,
     name VARCHAR(64) NOT NULL,
     active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
