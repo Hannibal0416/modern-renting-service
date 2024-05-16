d64887c31f845e8bff41d9d5c5213d73050adcd5
nyotie-tao <nyoto.wibowo@taodigitalusa.com>
2024-05-16 15:39:17 +0800

    Add brand, model, type entity

---
+++ vehicle-service/src/main/java/com/cdk/modern/renting/vehicleservice/domain/Brand.java
@@ -0,0 +1,33 @@
+package com.cdk.modern.renting.vehicleservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("vehicle_brand")
+public class Brand implements Serializable {
+    @Id
+    protected Integer id;
+
+    @Column
+    private String imageUri;
+
+    @Column
+    private String name;
+
+    @Column
+    private boolean active;
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+}
