033e67ad599e01240c9d7b2e289cd49e30996a9d
Jason Chou <jason.chou@taodigitalusa.com>
2024-05-20 16:21:50 +0800

    Implement API: update user

--- user-service/src/main/java/com/cdk/modern/renting/userservice/user/UserServiceImpl.java
+++ user-service/src/main/java/com/cdk/modern/renting/userservice/user/UserServiceImpl.java
@@ -1,41 +1,35 @@
 package com.cdk.modern.renting.userservice.user;
 
-import com.cdk.modern.renting.userservice.config.OAuth2Properties;
-import com.cdk.modern.renting.userservice.config.SecurityConfiguration;
 import com.cdk.modern.renting.userservice.domain.User;
 import com.cdk.modern.renting.userservice.user.request.UserCreateRequest;
 import com.cdk.modern.renting.userservice.user.request.UserUpdateRequest;
 
 import com.cdk.modern.renting.userservice.user.response.UserInfoResponse;
 
+import com.cdk.modern.renting.userservice.util.PasswordUtils;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.security.SecurityConfig;
-import org.springframework.beans.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataRetrievalFailureException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
 
-
   public UserInfoResponse getUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    User user = userRepository.findByUsername(authentication.getName()).orElseThrow(() -> new DataRetrievalFailureException("User not found"));
+    User user =
+        userRepository
+            .findByUsername(authentication.getName())
+            .orElseThrow(() -> new DataRetrievalFailureException("User not found"));
     return toCanonical(user);
   }
 
@@ -52,9 +46,25 @@ public class UserServiceImpl implements UserService{
     return null;
   }
 
+  @Transactional
   @Override
   public UserInfoResponse updateUser(UserUpdateRequest userUpdate) {
-    return null;
-  }
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    User updatedUser =
+        userRepository
+            .findByUsername(authentication.getName())
+            .map(
+                user -> {
+                  user.setEmail(userUpdate.getEmail());
+                  user.setPhone(userUpdate.getPhone());
 
+                  if (StringUtils.isNotBlank(userUpdate.getPassword())) {
+                    user.setPassword(PasswordUtils.prependNoop(userUpdate.getPassword()));
+                  }
+                  return userRepository.save(user);
+                })
+            .orElseThrow(() -> new DataRetrievalFailureException("User not found"));
+
+    return toCanonical(updatedUser);
+  }
 }
--- user-service/src/main/java/com/cdk/modern/renting/userservice/user/request/UserUpdateRequest.java
+++ user-service/src/main/java/com/cdk/modern/renting/userservice/user/request/UserUpdateRequest.java
@@ -1,11 +1,25 @@
 package com.cdk.modern.renting.userservice.user.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserUpdateRequest {
   private String password;
-  private String email;
-  private String phone;
 
+  @NotBlank(message = "Email is mandatory")
+  @Email(message = "Email should be valid")
+  private String email;
+
+  // Phone number format:
+  // 1. allows optional international code with prefix "+"
+  // 2. main phone number should be 10 digits
+  @NotBlank(message = "Phone number is mandatory")
+  @Pattern(
+      regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$",
+      message =
+          "Phone number should be valid, the format should be either 0900123456 or +10900123456")
+  private String phone;
 }
