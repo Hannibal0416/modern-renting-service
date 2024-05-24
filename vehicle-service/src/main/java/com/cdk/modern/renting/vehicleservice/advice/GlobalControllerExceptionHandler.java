a0eb05c38deac958b7bc01317e5b34ddde801b55
Hannibal.Han <hannibal.han@gmail.com>
2024-05-24 19:51:23 +0800

    fix errors

--- vehicle-service/src/main/java/com/cdk/modern/renting/vehicleservice/advice/GlobalControllerExceptionHandler.java
+++ vehicle-service/src/main/java/com/cdk/modern/renting/vehicleservice/advice/GlobalControllerExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.cdk.modern.renting.vehicleservice.advice;
 
+import org.springframework.security.access.AccessDeniedException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @ControllerAdvice
@@ -40,9 +40,9 @@ public class GlobalControllerExceptionHandler {
 
   @ExceptionHandler(RuntimeException.class)
   public Mono<ResponseEntity<ErrorResponse>> handleServerError(
-      RuntimeException ex, WebRequest request) {
-    log.error(ERR_TEMPLATE, ex, request);
-    List<String> errors = Arrays.asList(ex.getMessage(), request.getContextPath());
+      RuntimeException ex, ServerWebExchange exchange) {
+    log.error(ERR_TEMPLATE, ex, exchange);
+    List<String> errors = Arrays.asList(ex.getMessage(), exchange.getRequest().getPath().toString());
     return Mono.just(
         new ResponseEntity<>(
             ErrorResponse.builder().errors(errors).build(),
@@ -52,9 +52,9 @@ public class GlobalControllerExceptionHandler {
 
   @ExceptionHandler(RestClientException.class)
   public Mono<ResponseEntity<ErrorResponse>> handleGatewayError(
-      RuntimeException ex, WebRequest request) {
-    log.error(ERR_TEMPLATE, ex, request);
-    List<String> errors = Arrays.asList(ex.getMessage(), request.getContextPath());
+      RuntimeException ex, ServerWebExchange exchange) {
+    log.error(ERR_TEMPLATE, ex, exchange);
+    List<String> errors = Arrays.asList(ex.getMessage(), exchange.getRequest().getPath().toString());
     return Mono.just(
         new ResponseEntity<>(
             ErrorResponse.builder().errors(errors).build(),
@@ -64,9 +64,9 @@ public class GlobalControllerExceptionHandler {
 
   @ExceptionHandler(DataIntegrityViolationException.class)
   public Mono<ResponseEntity<ErrorResponse>> handleConflict(
-      RuntimeException ex, WebRequest request) {
-    log.error(ERR_TEMPLATE, ex, request);
-    List<String> errors = Collections.singletonList(ex.getMessage());
+      RuntimeException ex, ServerWebExchange exchange) {
+    log.error(ERR_TEMPLATE, ex, exchange);
+    List<String> errors = Arrays.asList(ex.getMessage(), exchange.getRequest().getPath().toString());
     return Mono.just(
         new ResponseEntity<>(
             ErrorResponse.builder().errors(errors).build(),
@@ -76,13 +76,25 @@ public class GlobalControllerExceptionHandler {
 
   @ExceptionHandler({DataRetrievalFailureException.class, NoSuchElementException.class})
   public Mono<ResponseEntity<ErrorResponse>> handleNotFound(
-      RuntimeException ex, WebRequest request) {
-    log.error(ERR_TEMPLATE, ex, request);
-    List<String> errors = Collections.singletonList(ex.getMessage());
+      RuntimeException ex, ServerWebExchange exchange) {
+    log.error(ERR_TEMPLATE, ex, exchange);
+    List<String> errors = Arrays.asList(ex.getMessage(), exchange.getRequest().getPath().toString());
     return Mono.just(
         new ResponseEntity<>(
             ErrorResponse.builder().errors(errors).build(),
             new HttpHeaders(),
             HttpStatus.NOT_FOUND));
+  }
+
+  @ExceptionHandler({AccessDeniedException.class})
+  public Mono<ResponseEntity<ErrorResponse>> handleNotFound(
+          AccessDeniedException ex, ServerWebExchange exchange) {
+    log.error(ERR_TEMPLATE, ex, exchange);
+    List<String> errors = Arrays.asList(ex.getMessage(), exchange.getRequest().getPath().toString());
+    return Mono.just(
+            new ResponseEntity<>(
+                    ErrorResponse.builder().errors(errors).build(),
+                    new HttpHeaders(),
+                    HttpStatus.FORBIDDEN));
   }
 }
